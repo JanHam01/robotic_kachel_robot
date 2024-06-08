e5020b15e753ae487ddfd23ca6398790180d3032
snens <janhampel91@gmail.com>
2024-06-08 15:36:05 +0200

    first workspace commit for simple dirac robot

---
+++ workspace/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/robotik/Documents/project/robotic_kachel_robot/workspace/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
